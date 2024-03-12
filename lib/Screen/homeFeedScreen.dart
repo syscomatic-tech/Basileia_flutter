420f1b435ffe6a5de44ff47c2649f7bf75961899
sadik-abd <sadikabdullah20000000000000@gmail.com>
2024-03-13 03:47:48 +0600

    fixed post

--- lib/Screen/homeFeedScreen.dart
+++ lib/Screen/homeFeedScreen.dart
@@ -11,167 +11,186 @@ import 'package:flutter/material.dart';
 import 'package:basileia/RestAPI/social.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart';
+import 'dart:async';
 
 class HomeFeedController extends GetxController {
-  List<Post> posts = [];
+  List<Post>? posts = [];
   var scl_client = SocialClient();
-  Future<void> call_posts() async {
+  Future<List<Post>> call_posts() async {
     final postss = await scl_client.get_all_posts();
-    posts.addAll(postss);
+    return postss;
   }
 
   @override
-  void onInit() async{
+  void onInit() async {
     super.onInit();
-    await call_posts();
+    unawaited(() async {
+      await Future.delayed(const Duration(seconds: 2))
+          .then((_) => print("Oops I am late"));
+    }.call());
+
+    call_posts();
   }
 }
 
 class HomeFeedScreen extends GetView<HomeFeedController> {
   final HomeFeedController controller = Get.put(HomeFeedController());
   List<Widget> feeds = [];
-  List<Post> posts = [];
+  List<Post>? posts = [];
   var scl_client = SocialClient();
 
   @override
   Widget build(BuildContext context) {
-    posts = controller.posts;
     //call_posts();
-    print(posts.length);
-    for (var post in posts) {
-      if (post.post_type < 2) {
-        if (!post.file_content
-            .toString()
-            .contains("https://api.zahedhasan.com/api/v1/uploads/users")) {
-          feeds.add(Feeds(
-            userName: post.usrName,
-            followers: post.followers.length.toString(),
-            likes: post.likes.length.toString(),
-            comments: post.comments.length.toString(),
-            postType: post.post_type,
-            content: post.file_content.toString(),
-            postID: post.id.toString(),
-          ));
-        }
-      } else {
-        // Audio and video feeds
-      }
-    }
+
     bool showFAB = MediaQuery.of(context).viewInsets.bottom != 0;
-    return Scaffold(
-      floatingActionButton: Visibility(
-        visible: !showFAB,
-        child: FloatingActionButton(
-          onPressed: () {
-            Get.to(() => PostOnFeed());
-          },
-          backgroundColor: primary,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30)),
-          ),
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 6.34, right: 6.34),
-          child: Column(
-            children: [
-              Container(
-                height: 310,
-                decoration: const BoxDecoration(
-                    color: primary,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(22.64),
-                        bottomRight: Radius.circular(22.64))),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 37, left: 20, right: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Image.asset(
-                            appLogo,
-                            width: 56,
-                          ),
-                          Row(
-                            children: [
-                              Profile(
-                                  onPressed: () {
-                                    Get.to(() => ProfileScreen());
-                                  },
-                                  OutSidewidth: 47,
-                                  OutSidehight: 47,
-                                  InsideWidth: 38,
-                                  InSideHight: 38),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              IconButton(
-                                onPressed: () {
-                                  Get.to(() => MenuScreen());
-                                },
-                                icon: Image.asset(menu),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 20, right: 220, left: 10),
-                      child: Text(
-                        'Wellcome,',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 24.46,
-                            color: Colors.white,
-                            fontFamily: poppins_semibold),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(right: 50),
-                      child: Text(
-                        'Consectetur adipiscing elit ',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20.6,
-                            color: Colors.white,
-                            fontFamily: poppins_regular),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    searchBar(),
-                    // Padding(
-                    //   padding: const EdgeInsets.only(top: 15, left: 250),
-                    //   child: AdvanceFilterButton(onTap: () async {
-                    //     var inboxscr = InboxScreen();
-                    //     await inboxscr.GetUsers();
-                    //     Get.to(() => inboxscr);
-                    //   }),
-                    // )
-                  ],
+    return FutureBuilder(
+        future: controller.call_posts(),
+        builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return CircularProgressIndicator();
+          } else {
+            if (snapshot.hasData) {
+              posts = snapshot.data;
+            } else {
+              posts = [];
+            }
+            for (var post in posts!) {
+              if (post.post_type < 2) {
+                if (!post.file_content.toString().contains(
+                    "https://api.zahedhasan.com/api/v1/uploads/users")) {
+                  feeds.add(Feeds(
+                    userName: post.usrName,
+                    followers: post.followers.length.toString(),
+                    likes: post.likes.length.toString(),
+                    comments: post.comments.length.toString(),
+                    postType: post.post_type,
+                    content: post.file_content.toString(),
+                    postID: post.id.toString(),
+                  ));
+                }
+              } else {
+                // Audio and video feeds
+              }
+            }
+            return Scaffold(
+              floatingActionButton: Visibility(
+                visible: !showFAB,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Get.to(() => PostOnFeed());
+                  },
+                  backgroundColor: primary,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: posts.length,
-                  itemBuilder: (context, index) {
-                    return feeds[index]; //feeds[index];
-                  })
-            ],
-          ),
-        ),
-      ),
-    );
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 6.34, right: 6.34),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 310,
+                        decoration: const BoxDecoration(
+                            color: primary,
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(22.64),
+                                bottomRight: Radius.circular(22.64))),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 37, left: 20, right: 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image.asset(
+                                    appLogo,
+                                    width: 56,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Profile(
+                                          onPressed: () {
+                                            Get.to(() => ProfileScreen());
+                                          },
+                                          OutSidewidth: 47,
+                                          OutSidehight: 47,
+                                          InsideWidth: 38,
+                                          InSideHight: 38),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      IconButton(
+                                        onPressed: () {
+                                          Get.to(() => MenuScreen());
+                                        },
+                                        icon: Image.asset(menu),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(
+                                  top: 20, right: 220, left: 10),
+                              child: Text(
+                                'Wellcome,',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 24.46,
+                                    color: Colors.white,
+                                    fontFamily: poppins_semibold),
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(right: 50),
+                              child: Text(
+                                'Consectetur adipiscing elit ',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 20.6,
+                                    color: Colors.white,
+                                    fontFamily: poppins_regular),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            searchBar(),
+                            // Padding(
+                            //   padding: const EdgeInsets.only(top: 15, left: 250),
+                            //   child: AdvanceFilterButton(onTap: () async {
+                            //     var inboxscr = InboxScreen();
+                            //     await inboxscr.GetUsers();
+                            //     Get.to(() => inboxscr);
+                            //   }),
+                            // )
+                          ],
+                        ),
+                      ),
+                      ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          primary: false,
+                          shrinkWrap: true,
+                          itemCount: posts!.length,
+                          itemBuilder: (context, index) {
+                            return feeds[index]; //feeds[index];
+                          })
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+        });
   }
 }
