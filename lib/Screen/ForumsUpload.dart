3fa9ffebed4fc330956dc85bca982267687be9c1
sadik-abd <sadikabdullah20000000000000@gmail.com>
2024-03-21 03:54:35 +0600

    Made some changes

---
+++ lib/Screen/ForumsUpload.dart
@@ -0,0 +1,141 @@
+import 'package:basileia/Screen/homeFeedScreen.dart';
+import 'package:basileia/Screen/forumsScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:basileia/RestAPI/RestClient.dart';
+import 'package:basileia/Style/images.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:io';
+import '../Style/colors.dart';
+import '../Style/controller.dart';
+import '../Style/fonts.dart';
+import '../Style/style.dart';
+
+class ForumsUpload extends StatelessWidget {
+  final TabsController controller_1 = Get.put(TabsController());
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  arrowButton(),
+                  const Text(
+                    'New Post',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontFamily: poppins_semibold),
+                  ),
+                  button(
+                      Width: 72,
+                      Height: 45,
+                      onTap: () async {
+                        if (_controller.text.isNotEmpty) {
+                          SocialClient scl_cl = SocialClient();
+                          await uploadForumPost(_controller.text, "GN");
+                        } else {
+                          SocialClient scl_cl = SocialClient();
+                          await uploadForumPost("", "GN");
+                        }
+                        SuccessToast("post uploaded");
+                        Get.to(() => ForumsScreen());
+                      },
+                      text: 'Post'),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Divider(
+              color: bordar,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.maxFinite,
+              height: 500,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 273,
+                      width: 373,
+                      decoration: BoxDecoration(
+                          color: TabBG,
+                          borderRadius: BorderRadius.circular(19)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 20),
+                        child: TextFormField(
+                          controller: _controller,
+                          maxLines: null,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'What’s On Your Mind',
+                              hintStyle: TextStyle(
+                                fontSize: 15,
+                                fontFamily: poppins_regular,
+                              )),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Post In :',
+                          style: TextStyle(
+                              fontFamily: poppins_regular,
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        textField(
+                            width: 256,
+                            hight: 48,
+                            lebelText: 'General',
+                            suffixIcon: Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: InkWell(
+                                  onTap: () {},
+                                  child: Image.asset(Downarrow_ic)),
+                            ),
+                            textfieldBg: TabBG),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screen/forumsScreen.dart
+++ lib/Screen/forumsScreen.dart
@@ -1,8 +1,9 @@
-
 import 'package:basileia/Screen/questionDetailsScreen.dart';
 import 'package:basileia/Style/colors.dart';
 import 'package:basileia/Style/images.dart';
 import 'package:basileia/Style/style.dart';
+import 'package:basileia/RestAPI/RestClient.dart';
+import 'package:basileia/RestAPI/social.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,115 +13,150 @@ class ForumsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bool showFAB = MediaQuery.of(context).viewInsets.bottom != 0;
-    return Scaffold(
-      floatingActionButton: Visibility(
-        visible: !showFAB,
-        child: FloatingActionButton(onPressed: (){Get.to(()=>const QuestionDetailScreen());},shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        ),backgroundColor: primary,child: const Icon(Icons.add,color: Colors.white,),),
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height * 100,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [gradient_1, gradient],
-            stops: [0.0, 1.0],
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 15,
-              right: 15,
-            ),
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Forums',
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w600),
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Ask Question',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      InkWell(onTap: () {}, child: Image.asset(notification))
-                    ],
+    List<Question> questions = [];
+    return FutureBuilder(
+        future: getForumPosts(),
+        builder: (context, AsyncSnapshot<List<Question>> snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Scaffold(
+                body: Center(
+                    child: SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: CircularProgressIndicator())));
+          } else {
+            if (snapshot.hasData) {
+              questions = snapshot.data!;
+            } else {
+              print(snapshot);
+              questions = [];
+            }
+            return Scaffold(
+              floatingActionButton: Visibility(
+                visible: !showFAB,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    // Get.to(() => const QuestionDetailScreen());
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  backgroundColor: primary,
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
+              ),
+              body: Container(
+                height: MediaQuery.of(context).size.height * 100,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [gradient_1, gradient],
+                    stops: [0.0, 1.0],
+                  ),
                 ),
-                forumScreen_searchBar(),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Categories',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                      right: 15,
                     ),
-                    seeAllButton(onTap: () {})
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    categories(onTap: () {}, text: 'General'),
-                    categories(onTap: () {}, text: 'Bible'),
-                    categories(onTap: () {}, text: 'Question'),
-                    categories(onTap: () {}, text: 'Motivation'),
-                  ],
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Latest Question',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 50),
+                          child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Forums',
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w600),
+                              )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 30),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'Ask Question',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              InkWell(
+                                  onTap: () {},
+                                  child: Image.asset(notification))
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        forumScreen_searchBar(),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Categories',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            seeAllButton(onTap: () {})
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            categories(onTap: () {}, text: 'General'),
+                            categories(onTap: () {}, text: 'Bible'),
+                            categories(onTap: () {}, text: 'Question'),
+                            categories(onTap: () {}, text: 'Motivation'),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Latest Question',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            seeAllButton(onTap: () {})
+                          ],
+                        ),
+                        ListView.builder(
+                          primary: false,
+                          shrinkWrap: true,
+                          itemCount: questions.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return question(
+                              username: questions[index].usrName,
+                              contentType: questions[index].type,
+                              content: questions[index].content,
+                            );
+                          },
+                        )
+                      ],
                     ),
-                    seeAllButton(onTap: () {})
-                  ],
+                  ),
                 ),
-                ListView.builder(
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index) {
-                    return question(username: '',contentType: '',content: '',);
-                  },
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+              ),
+            );
+          }
+        });
   }
 }
